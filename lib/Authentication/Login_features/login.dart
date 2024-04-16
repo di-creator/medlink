ef5888d1f4a1079e9560d0b6192a751fe93e778b
di-creator <yorkedianaba@gmail.com>
2024-04-15 17:07:34 -0700

    updates made on 14thApril24

--- lib/Authentication/Login_features/login.dart
+++ lib/Authentication/Login_features/login.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:ionicons/ionicons.dart';
+import 'package:medlink/Authentication/Login_features/forgotpassword.dart';
 import 'package:medlink/Authentication/Signup%20features/signupscreen.dart';
+import 'package:medlink/home_pages/home_screen.dart';
 import 'package:medlink/widgets/custom%20shapes/Authetication_widgets/toplogo.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -11,8 +15,66 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  String email = "", password = "";
+
+  TextEditingController emailcontroller = new TextEditingController();
+  TextEditingController passwordcontroller = new TextEditingController();
+
+  final _formkey = GlobalKey<FormState>();
+
   bool _passwordVisible = false;
   bool _rememberMe = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadSavedCredentials();
+  }
+
+  _loadSavedCredentials() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      email = prefs.getString('email') ?? '';
+      password = prefs.getString('password') ?? '';
+      _rememberMe = prefs.getBool('rememberMe') ?? false;
+    });
+    if (_rememberMe) {
+      emailcontroller.text = email;
+      passwordcontroller.text = password;
+    }
+  }
+
+  _saveCredentials() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('email', email);
+    await prefs.setString('password', password);
+    await prefs.setBool('rememberMe', _rememberMe);
+  }
+
+  userLogin() async {
+    try {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => Homepage()));
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            backgroundColor: const Color.fromRGBO(0, 122, 135, 100),
+            content: Text(
+              "No User Found for that Email",
+              style: TextStyle(fontSize: 18.0),
+            )));
+      } else if (e.code == 'wrong-password') {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            backgroundColor: const Color.fromRGBO(0, 122, 135, 100),
+            content: Text(
+              "Wrong Password Provided by User",
+              style: TextStyle(fontSize: 18.0),
+            )));
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,10 +87,18 @@ class _LoginScreenState extends State<LoginScreen> {
               const TopLogoIntro(),
               const SizedBox(height: 50),
               Form(
+                key: _formkey,
                 child: Column(
                   children: [
                     // Email
                     TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please Enter E-mail';
+                        }
+                        return null;
+                      },
+                      controller: emailcontroller,
                       decoration: const InputDecoration(
                         prefixIcon: Icon(Ionicons.mail_open_outline),
                         labelText: 'Email',
@@ -39,6 +109,13 @@ class _LoginScreenState extends State<LoginScreen> {
                     // Password
                     const SizedBox(height: 20),
                     TextFormField(
+                      controller: passwordcontroller,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please Enter Password';
+                        }
+                        return null;
+                      },
                       decoration: InputDecoration(
                         prefixIcon: const Icon(Icons.password),
                         labelText: 'Password',
@@ -83,7 +160,11 @@ class _LoginScreenState extends State<LoginScreen> {
                         ),
                         TextButton(
                           onPressed: () {
-                            // Add action for forgot password
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        ForgotPassword())); // Add action for forgot password
                           },
                           child: const Text(
                             'Forgot password?',
@@ -171,6 +252,10 @@ class _LoginScreenState extends State<LoginScreen> {
                       child: ElevatedButton(
                         onPressed: () {
                           // Handle button tap
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => Homepage()));
                         },
                         style: ElevatedButton.styleFrom(
                           backgroundColor:
--- lib/home_pages/home_screen.dart
+++ lib/home_pages/home_screen.dart
@@ -2,8 +2,7 @@ import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:ionicons/ionicons.dart';
 import 'package:medlink/home_pages/hamburger_nav.dart';
-
-
+import 'package:table_calendar/table_calendar.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
@@ -11,45 +10,251 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer:const DrawerNav(),
-    
-      
+      drawer: const DrawerNav(),
       appBar: AppBar(
         title: const Column(
-          children: [
-            Text('')
-          ],
+          children: [Text('')],
         ),
         actions: [
           IconButton.outlined(
-            onPressed: (){}, 
-            icon: const Icon(Icons.notifications_outlined)
-            
-            )
-        ],
-
-      ),
-      body: ListView(
-        padding: const EdgeInsets.all(14),
-        children: const [
-
+              onPressed: () {}, icon: const Icon(Icons.notifications_outlined))
         ],
       ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // Search bar with voice search icon
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Search anything here...',
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.mic,
+                          color: Color.fromRGBO(0, 122, 135, 1)),
+                      onPressed: () {
+                        // Implement voice search functionality
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20.0),
+              // Stack to overlay elevated button on the card
+              Stack(
+                children: [
+                  // Card with user greeting and profile image
+                  Container(
+                    width: 361,
+                    height: 166,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.0),
+                      color: Color.fromRGBO(0, 122, 135, 1),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          CircleAvatar(
+                            radius: 30.0,
+                            backgroundImage: NetworkImage(
+                                'https://via.placeholder.com/150'), // Add user profile image URL here
+                          ),
+                          SizedBox(width: 20.0),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Hello User,',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(height: 5.0),
+                              Text(
+                                'How are you feeling today?',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16.0,
+                                ),
+                              ),
+                              SizedBox(height: 5.0),
+                              Text(
+                                'Need a doctor or consultant?',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16.0,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  // Elevated button for booking appointment
+                  Positioned(
+                    bottom: 10.0,
+                    left: 10.0,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Add functionality to book appointment
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        minimumSize: Size(192, 42),
+                        backgroundColor: Color(0xFFFDC0D4),
+                      ),
+                      child: Text('Book Appointment'),
+                    ),
+                  ),
+                ],
+              ),
 
+              SizedBox(height: 20.0),
+              // Second card with medication information
+              Container(
+                width: 361,
+                height: 188,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.0),
+                  color: Color.fromRGBO(0, 122, 135, 1),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Row with asset image and text side by side
+                      Row(
+                        children: [
+                          Image.asset(
+                            'assets/images/medicines.png',
+                            width: 131,
+                            height: 113,
+                          ),
+                          SizedBox(
+                              width: 20.0), // Add space between image and text
+                          Flexible(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Need to get some medications?',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16.0,
+                                  ),
+                                ),
+                                SizedBox(height: 10.0),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    // Add functionality for pharmacy button
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    minimumSize: Size(142, 42),
+                                    backgroundColor: Color(0xFFFDC0D4),
+                                  ),
+                                  child: Text('Pharmacy'),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
 
+              SizedBox(height: 20.0),
+              // Second card with medication information
+              Container(
+                width: 361,
+                height: 188,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.0),
+                  color: Color.fromRGBO(0, 122, 135, 1),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Row with asset image and text side by side
+                      Row(
+                        children: [
+                          Image.asset(
+                            'assets/images/records.png',
+                            width: 131,
+                            height: 113,
+                          ),
+                          SizedBox(
+                              width: 20.0), // Add space between image and text
+                          Flexible(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Need to get some medications?',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16.0,
+                                  ),
+                                ),
+                                SizedBox(height: 10.0),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    // Add functionality for pharmacy button
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    minimumSize: Size(142, 42),
+                                    backgroundColor: Color(0xFFFDC0D4),
+                                  ),
+                                  child: Text('Records'),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              // Book Appointment Button
+            ],
+          ),
+        ),
+      ),
       bottomNavigationBar: CurvedNavigationBar(
         backgroundColor: Colors.white,
-        color: Colors.greenAccent,
+        color: Color.fromRGBO(0, 122, 135, 100),
         animationDuration: const Duration(milliseconds: 300),
-        
         items: const [
           Icon(Ionicons.home_outline),
           Icon(Ionicons.calendar_outline),
           Icon(Ionicons.folder_open_outline),
           Icon(Ionicons.medkit_outline),
-
-      ],),
-      
+        ],
+      ),
     );
   }
 }
